8a4ea105638da2316cf4b8dc5a3349ccfc0d7ce8
mellemhere <gabriel@mellemhere.com>
2020-02-25 20:17:33 -0300

    Pagina de selecao de cor

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import iro from '@jaames/iro';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+    private colorWheel;
+    private hotColors = [
+        '#FFFFFF', '#FF0000', '#7CFC00', '#FFA500', '#FFFF00', '#FF00FF', '#00FFFF', '#00BFFF', '#0000FF', '#800000'
+    ];
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+        this.colorWheel = iro.ColorPicker('#colorWheelDemo');
+    }
+
+    public changeColor(color) {
+        this.colorWheel.color.hexString = color;
+    }
 
 }
